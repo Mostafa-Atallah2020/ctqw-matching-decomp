0e31293730496cdc074d0af456e126fb55cee7ba
Mostafa Atallah <m.ataallah.2014@gmail.com>
2024-05-11 11:36:06 -0400

    Allow for circuit simplifications

--- src/mcrx.py
+++ src/mcrx.py
@@ -1,7 +1,9 @@
+import re
+
 import numpy as np
 from qiskit import QuantumCircuit
 from qiskit.circuit.parametervector import ParameterVector
-from sympy import Not, Or
+from sympy import Not, Or, simplify
 
 from src.misc import multi_crx
 
@@ -12,36 +14,43 @@ class MCRX:
         self.n_qubits = n_qubits
         self.target = target
         self.t = ParameterVector("t")
+        self.ctrls = []
         self.__get_ctrls()
-        self.__get_mcrx_gates()
         self.__get_qc()
 
     def __get_ctrls(self):
-        self.ctrls = []
         for term in self.expr.args:
             ctrl_state = ""
+            oper = []
             for subterm in term.args:
                 if isinstance(subterm, Not):
                     ctrl_state += "0"  # append 0 for negation
+                    name = subterm.args[0].name
                 else:
                     ctrl_state += "1"  # append 1 for non-negated variable
+                    name = subterm.name
 
-            if ctrl_state in self.ctrls:
-                self.ctrls.append(ctrl_state[::-1])
-            else:
-                self.ctrls.append(ctrl_state)
+                number = re.search(r"\d+", name).group()
+                oper.append(int(number))
 
-    def __get_mcrx_gates(self):
-        self.mcrx_gates = []
-        angle = np.pi / 2  # TODO: make it for a general angle
-        for ctrl_state in self.ctrls:
-            gate = multi_crx(angle, ctrl_state)
-            self.mcrx_gates.append(gate)
+            self.ctrls.append((ctrl_state, oper))
 
     def __get_qc(self):
+        angle = np.pi / 2  # TODO: make it for a general angle.
         self.qc = QuantumCircuit(self.n_qubits)
-        for gate in self.mcrx_gates:
-            if gate.num_qubits != self.n_qubits:
-                raise ValueError("Enter a valid expression!")
+        for tuple in self.ctrls:
+            ctrl_state = tuple[0]
+            oper = tuple[1]
+            gate = multi_crx(angle, ctrl_state)
+            self.qc.append(gate, oper + [self.target])
 
-            self.qc.append(gate, range(self.n_qubits))
+    def simplify(self):
+        if len(self.expr.args) % 2 == 0:
+            self.expr = simplify(self.expr)
+        else:
+            first = self.expr.args[0]
+            all_except_first = Or(*self.expr.args[1:])
+            simplified_terms = simplify(all_except_first)
+            self.expr = simplified_terms | first
+
+        return MCRX(self.expr, self.n_qubits, self.target)
