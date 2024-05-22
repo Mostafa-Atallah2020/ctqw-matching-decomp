424b4cc29010c665906c4c190fc33852e5dd6e66
Mostafa Atallah <m.ataallah.2014@gmail.com>
2024-05-21 22:22:04 -0400

    Make proper imports

--- test/test_mcrx.py
+++ test/test_mcrx.py
@@ -1,14 +1,10 @@
-import sys
-
-sys.path.append(f"./../")
 import numpy as np
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Operator
 from sympy import symbols
 
 # from src import MCRX, multi_crx
-from src.mcrx import MCRX
-from src.misc import multi_crx
+from src import MCRX, multi_crx
 
 
 def test_3_qubit_4_parallel_edges_expr():
