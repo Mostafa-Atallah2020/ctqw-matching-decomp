c23bd53f31c73ce7d1e012337c5414811bac4e85
Mostafa Atallah <m.ataallah.2014@gmail.com>
2024-06-08 01:09:21 -0400

    Define validation methods for static graphs

--- src/graphs.py
+++ src/graphs.py
@@ -8,16 +8,32 @@ from src.misc import binary_tuple_to_int_tuple
 
 
 class StaticGraph:
-    def __init__(self, n_qubits, edges) -> None:
-        self.n_qubits = n_qubits
-        self.nodes = set(range(2**n_qubits))
+    def __init__(self, edges) -> None:
+        self._validate_edges(edges)
         self.edges = edges
+        self.n_qubits = len(next(iter(edges))[0])
+        self.nodes = set(range(2**self.n_qubits))
         self.rot_angle = self.__get_rot_angle()
         self.__int_edges = set([binary_tuple_to_int_tuple(t) for t in self.edges])
 
         self.graph = nx.Graph()
         self.graph.add_nodes_from(self.nodes)
         self.graph.add_edges_from(self.edges)
+
+    def _validate_edges(self, edges):
+        if not isinstance(edges, set):
+            raise ValueError("Edges must be a set")
+        edge_length = None
+        for edge in edges:
+            if not isinstance(edge, tuple) or len(edge) != 2:
+                raise ValueError("Each edge must be a tuple of length 2")
+            for node in edge:
+                if not isinstance(node, str) or not all(bit in '01' for bit in node):
+                    raise ValueError("Each node in an edge must be a string consisting of 0s and 1s")
+                if edge_length is None:
+                    edge_length = len(node)
+                elif len(node) != edge_length:
+                    raise ValueError("All nodes in edges must have the same length")
 
     def __add__(self, other):
         return StaticGraph(self.nodes | other.nodes, self.edges | other.edges)
@@ -46,14 +62,15 @@ class StaticGraph:
 
 
 class ParallelEdgeGraph(StaticGraph):
-    def __init__(self, n_qubits, edges):
-        super().__init__(n_qubits, edges)
+    def __init__(self, edges):
+        super().__init__(edges)
         self.target = None
         self.vars = self.__get_vars()
         self.expr = self.__get_expr()
 
     def get_qc(self, simplified=False):
         mcrx = MCRX(self.n_qubits, self.expr, self.target, self.rot_angle)
+        
         if simplified:
             return mcrx.simplify().qc
         else:
