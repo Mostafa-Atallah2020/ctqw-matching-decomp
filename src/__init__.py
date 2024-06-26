3e886df4c39179e6152a17265809db8543befa09
Mostafa Atallah <m.ataallah.2014@gmail.com>
2024-06-26 12:23:22 -0400

    Define Expression class

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
-from src.edges import Edge
+from src.edge import Edge
+from src.expression import Expression
 from src.graph_drawer import GraphDrawer
 from src.mcrx import MCRX
 from src.misc import multi_crx
